4ad1e1347615fdd47836efb4b8049a32ff3a50d8
the8projects <the8projects@gmail.com>
2020-02-10 23:20:59 +0700

    Creating Project - try to rotate img with button

---
+++ Assets/_script/BtnLeftController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BtnLeftController : MonoBehaviour
+{
+    private bool _rotateLeft;
+    public bool rotateLeft
+    {
+        get { return _rotateLeft; }
+        set { _rotateLeft = value; }
+    }
+    
+    public void OnPress()
+    {
+        rotateLeft = true;
+        Debug.Log("LeftPressed");
+    }
+
+    public void OnRelease()
+    {
+        rotateLeft = false;
+    }
+}
---
+++ Assets/_script/BtnRightController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BtnRightController : MonoBehaviour
+{
+    private bool _rotateRight;
+    public bool rotateRight
+    {
+        get { return _rotateRight; }
+        set { _rotateRight = value; }
+    }
+    
+    public void OnPress()
+    {
+        rotateRight = true;
+        Debug.Log("RightPressed");
+    }
+
+    public void OnRelease()
+    {
+        rotateRight = false;
+    }
+}
