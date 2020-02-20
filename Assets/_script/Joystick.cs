3d46b02f4996e735c377ab45c552e002cd166c3f
the8projects <the8projects@gmail.com>
2020-02-20 23:52:12 +0700

    Added - Rotate Player with Joystick

--- Assets/_script/Joystick.cs
+++ Assets/_script/Joystick.cs
@@ -35,6 +35,7 @@ public class Joystick : MonoBehaviour
             pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
         }else{
             touchStart = false;
+            
         }
     }
 
@@ -42,11 +43,11 @@ public class Joystick : MonoBehaviour
     {
         if (touchStart)
         {
-            Vector2 offset = pointB - pointA;
-            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            Vector2 direction = pointB - pointA;
+            Vector2 offset = Vector2.ClampMagnitude(direction, 1.0f);
             moveCharacter(direction);
             
-            circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y);
+            circle.transform.position = new Vector2(pointA.x + offset.x, pointA.y + offset.y);
             //player.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y);
         }else{
             circle.GetComponent<SpriteRenderer>().enabled = false;
@@ -60,8 +61,8 @@ public class Joystick : MonoBehaviour
         //player.eulerAngles = new Vector3( 0, Mathf.Atan2(Input.mousePosition.x, Input.mousePosition.y) * 180 / Mathf.PI, 0 );
         //Quaternion target = Quaternion.Euler((Input.mousePosition.x * 60.0f), 0, (Input.mousePosition.y * 60.0f));
         //player.rotation = Quaternion.Slerp(player.rotation, target,  Time.deltaTime * speed);
-        //float heading = Mathf.Atan2(Input.mousePosition.x,Input.mousePosition.y) * -1;
-        //player.rotation = Quaternion.Euler(0f,0f,heading * Mathf.Rad2Deg);
+        float heading = Mathf.Atan2((direction.x),(direction.y)) * -1;
+        player.rotation = Quaternion.Euler(0f,0f,heading * Mathf.Rad2Deg);
 
         //Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //Vector2 direction2 = (mouseWorldPosition - (Vector2) transform.position).normalized;
